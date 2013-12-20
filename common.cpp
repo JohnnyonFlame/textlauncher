ae1492430b8c965eb2bc2cf7994bc17db14e7b74
Johnny <johnnyonflame@hotmail.com>
2013-12-20 15:49:55 -0200

    Forgot a couple files.

---
+++ common.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <SDL.h>
+#include "textscreen.h"
+
+#include "common.h"
+
+int systemf(const char *fmt, ...)
+{
+        char __sys[1024];
+        int ret_code;
+        va_list args;
+        va_start(args, fmt);
+                vsnprintf(__sys, 1023, fmt, args);
+                ret_code = system(__sys);
+        va_end(args);
+
+        return ret_code;
+}
---
+++ common.h
@@ -0,0 +1,6 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+
+int systemf(const char *fmt, ...);
+
+#endif /* COMMON_H_ */
